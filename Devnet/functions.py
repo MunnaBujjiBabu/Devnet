8cc37d2696da0d664848adecc91c78fa8bc383b5
MunnaBujjiBabu <munnabujjireddy@gmail.com>
2022-06-12 23:56:55 +0530

    Update functions.py

--- Devnet/functions.py
+++ Devnet/functions.py
@@ -2,3 +2,13 @@ def sub(arg1,arg2):
     result = arg1-arg2
     return result
 print (sub(10,5))
+
+def hello(*args):
+    for arg in args:
+        print("hello", arg)
+hello("asdfasd", "asdf" , "qwerqwe")
+
+def hello(**kwargs):
+    for key, value in kwargs.items():
+        print("hello", key, " ", value)
+hello(t1="aaaa", t2="bbbb")
