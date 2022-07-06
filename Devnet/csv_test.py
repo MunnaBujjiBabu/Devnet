0180d50f31db073734c69e440b42f97e70985cca
MunnaBujjiBabu <munnabujjireddy@gmail.com>
2022-07-06 19:43:51 +0530

    csv

--- Devnet/csv_test.py
+++ Devnet/csv_test.py
@@ -149,4 +149,4 @@ def palindrome(word):
     if len(word) <= 1:
         return True
     else:
-        return word[0] == word[-1] and palindrome(word[1:-1])   
+        return word[0] == word[-1] and palindrome(word[1:-1])
